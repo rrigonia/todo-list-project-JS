46155b346597b0fc55465512ea59f0452c61af67
rrigonia <83792487+rrigonia@users.noreply.github.com>
2021-05-24 21:16:09 -0300

    first try

---
+++ main.js
@@ -0,0 +1,30 @@
+let todo = [];
+
+let firstOne = prompt("Welcome, type the first Todo of your list")
+todo[0] = firstOne;
+let choice = prompt("What u want to do ?")
+console.log(todo);
+
+while (choice.toLowerCase() !== "quit") {
+
+    if (choice.toLowerCase() === "new") {
+        let add = prompt("What u want to add ?");
+        todo.push(add);
+    } else if (choice.toLowerCase() === "list") {
+        let i = 0;
+        console.log("*********");
+        for (let things of todo) {
+            console.log(`${i}: ${things}`);
+            i++;
+        }
+        console.log("*********");
+
+    } else if (choice.toLowerCase() === "delete") {
+        let remove = prompt("Which one u want to remove ?");
+        todo.splice(remove, 1);
+    }
+
+    choice = prompt("OK, Done! So, What u want to do now ?")
+}
+
+
